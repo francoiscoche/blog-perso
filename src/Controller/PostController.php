973d897dca3f1ea22898bb4c49b426c930d90daa
francois <francois.coche@outlook.it>
2022-12-17 18:05:08 +0100

    ✨pagination on posts page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -17,12 +19,19 @@ class PostController extends AbstractController
      * @return Response
      */
     #[Route('/posts', name: 'app_post', methods:['GET'])]
-    public function index(PostRepository $repo): Response
+    public function index(PostRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
-        $posts = $repo->findAll();
+        // $query = $repo->findAll();
+        $query = $repo->findPosts();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
         
         return $this->render('pages/post/index.html.twig', [
-            'posts' => $posts,
+            'pagination' => $pagination,
         ]);
     }
 
