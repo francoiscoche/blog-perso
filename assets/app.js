41dc6fdbaa99104b7e92bb986e125de479a58435
francois <francois.coche@outlook.it>
2023-01-25 19:06:33 +0100

    🎨add dark mode new icon

--- assets/app.js
+++ assets/app.js
@@ -16,15 +16,27 @@ import 'tw-elements';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
-document.querySelector("#theme").addEventListener("click", function() {
+    const element = document.getElementById("icon-sun-moon");
+
+    if(localStorage.getItem("checkedbox") == 'true') {
+        element.classList.add("fas","fa-sun");
+    } else {
+        element.classList.add("fas","fa-moon");
+    }
+
+document.querySelector("#button-switch-theme-icon").addEventListener("click", function() {
+    const element = document.getElementById("icon-sun-moon");
+
     if(localStorage.getItem("checkedbox") == 'true') {
         localStorage.setItem("checkedbox", 'false');
         localStorage.theme = 'light'
         document.querySelector("html").classList.remove("dark");
+        element.classList.add("fas","fa-moon");
     }
     else {
         localStorage.setItem("checkedbox", 'true');
         localStorage.theme = 'dark'
-        document.querySelector("html").classList.add("dark")
+        document.querySelector("html").classList.add("dark");
+        element.classList.add("fas","fa-sun");
     }
 })
